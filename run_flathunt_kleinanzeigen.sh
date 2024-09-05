18bed4b95f571b804eeb13e25d8a8778e1ae25b0
Vadim Petrov <vpetrov@n-ix.com>
2024-09-05 21:18:41 +0200

    Move scripts logit to cron config

--- run_flathunt_kleinanzeigen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /root/.local/share/virtualenvs/root-BuDEOXnJ/bin/activate
-pipenv run /root/.local/share/virtualenvs/root-BuDEOXnJ/bin/python /root/flathunter/flathunt.py --config /root/flathunter/config_kleinanzeigen.yaml
