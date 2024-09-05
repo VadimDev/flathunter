49d3a9e267a5f0a28dfabe7ee5b549580f7b06a8
Vadim Petrov <vpetrov@n-ix.com>
2024-09-05 16:33:06 +0200

    Change google chrome to chromium

--- flathunter/chrome_wrapper.py
+++ flathunter/chrome_wrapper.py
@@ -1,6 +1,3 @@
-"""Chrome needs some special handling to work out where the correct
-binary is, to attach the correct selenium chromedriver, and to set
-the correct version number"""
 import re
 import subprocess
 from typing import List
@@ -41,24 +38,16 @@ def get_chrome_version() -> int:
             return int(match.group(1).split('.')[0])
         except FileNotFoundError:
             pass
-    try:
-        # on Windows, Chrome doesn't respond to --version, but we can find
-        # the version in the registry
-        output = get_command_output(
-            ['reg', 'query', WINDOWS_CHROME_REG_PATH, '/v', 'version']
-        )
-        version_matches = (WINDOWS_CHROME_REG_REGEXP.match(l) for l in output)
-        version_matches = [m for m in version_matches if m is not None]
-        if version_matches:
-            return int(version_matches[0].group(1))
-    except FileNotFoundError:
-        pass
     raise ChromeNotFound()
 
 def get_chrome_driver(driver_arguments):
     """Configure Chrome WebDriver"""
     logger.info('Initializing Chrome WebDriver for crawler...')
-    chrome_options = uc.ChromeOptions() # pylint: disable=no-member
+    chrome_options = uc.ChromeOptions()  # pylint: disable=no-member
+
+    # Указываем путь к бинарнику Chromium
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
+
     if platform == "darwin":
         chrome_options.add_argument("--headless")
     if driver_arguments is not None:
@@ -66,7 +55,7 @@ def get_chrome_driver(driver_arguments):
             chrome_options.add_argument(driver_argument)
     chrome_version = get_chrome_version()
     chrome_options.add_argument("--headless=new")
-    driver = uc.Chrome(version_main=chrome_version, options=chrome_options) # pylint: disable=no-member
+    driver = uc.Chrome(version_main=chrome_version, options=chrome_options)  # pylint: disable=no-member
 
     driver.execute_cdp_cmd(
         "Network.setUserAgentOverride",
