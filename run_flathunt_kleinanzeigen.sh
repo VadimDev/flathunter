f0cd831639c2392ae2893208457ca2062d9e7454
Vadim Petrov <vpetrov@n-ix.com>
2024-09-05 21:08:51 +0200

    Add full path for cron task

--- run_flathunt_kleinanzeigen.sh
+++ run_flathunt_kleinanzeigen.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source /root/.local/share/virtualenvs/root-BuDEOXnJ/bin/activate
 pipenv run /root/.local/share/virtualenvs/root-BuDEOXnJ/bin/python /root/flathunter/flathunt.py --config /root/flathunter/config_kleinanzeigen.yaml
