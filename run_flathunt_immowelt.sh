22880e3c381254699817c6a1bf0b62acad3111fc
Vadim Petrov <vpetrov@n-ix.com>
2024-09-05 20:07:40 +0200

    change .sh file for cron autorun

--- run_flathunt_immowelt.sh
+++ run_flathunt_immowelt.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-while true; do
-    pipenv run python flathunt.py --config config_immowelt.yaml &
-    PID=$!
-
-    sleep 300
-
-    kill $PID 2>/dev/null
-
-    wait $PID 2>/dev/null
-
-    echo "Restart..."
-done
+pipenv run python flathunt.py --config config_immowelt.yaml
